5b101cf368752b6e53f5a92c2bbba0d03a57be4f
Tycoon19 <kevin960831@naver.com>
2021-12-16 16:18:31 +0900

    저장된 자동로그인 여부 확인 기능 추가

--- app/src/main/java/com/example/sharedpreftest_jaewhi/utils/ContextUtil.kt
+++ app/src/main/java/com/example/sharedpreftest_jaewhi/utils/ContextUtil.kt
@@ -17,6 +17,17 @@ class ContextUtil {
             pref.edit().putBoolean(AUTO_LOGIN, autoLogin).apply()
 
         }
+//      메모장을 열기만 하면 되니깐 인자가 context밖에 없다. 그대신 결과를 ox로 가져올거야~라는 의미로 : Boolean을 추가해준다.
+        fun getAutoLogin(context: Context) : Boolean{
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+//          원래 getBoolean("AUTO_LOGIN")으로 해야하는데 여기서 오타가 나면 꼬이기 때문에 위에서 선언해준 변수를 대입해준다.
+//          처음 앱을 켰을때 디폴트는 뭐야? -> 안누른거~ -> false
+            return pref.getBoolean(AUTO_LOGIN, false)
+
+
+
+        }
 
     }
 
